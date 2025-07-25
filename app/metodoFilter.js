2da644e8ecd6e8b1a9556844f4a609883e66030f
MatheusMatiasUniver <matheusactwind@outlook.com>
2025-07-24 22:17:12 -0300

    implementado metodo reduce

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -9,7 +9,8 @@ function filtroLivros() {
     exibirLivros(livrosFiltrados);
 
     if (categoria == 'disponivel') {
-        exibirValorTotalNaTela();
+        const valorTotal = calcularValorTotalLivrosDisponiveis(livrosFiltrados);
+        exibirValorTotalNaTela(valorTotal);
     }
 }
 
@@ -21,10 +22,10 @@ function filtrarPorDisponibilidade() {
     return livros.filter(livro => livro.quantidade > 0);
 }
 
-function exibirValorTotalNaTela() {
+function exibirValorTotalNaTela(valorTotal) {
     valorTotalDeLivrosDiponiveis.innerHTML = `
     <div class="livros__disponiveis">
-      <p>Todos os livros disponíveis por R$ <span id="valor">299,00</span></p>
+      <p>Todos os livros disponíveis por R$ <span id="valor">${valorTotal}</span></p>
     </div>
     `
 }
